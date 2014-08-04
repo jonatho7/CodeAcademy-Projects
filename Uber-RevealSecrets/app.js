fcf7c788dbaa743eb71a495b3dc6900aeed027a7
Jonathon Hellmann <jonatho7@vt.edu>
2014-08-03 22:34:53 -0400

    Created Uber-RevealSecrets web page

---
+++ Uber-RevealSecrets/app.js
@@ -0,0 +1,68 @@
+//This defines the main function.
+var main = function() {
+    //$( ) tells the computer we're using jQuery.
+    //'.icon-menu' uses CSS to select the class called '.icon-menu'
+    //.click() means something will happen when the user clicks on the icon-menu HTML element.
+    $('.icon-menu').click(function() {
+        //animate method. menu's left offset will be set to equal to 0px, over 200ms.
+        //Use a period to select a class.
+        $('.menu').animate({
+            left: '0px'
+        }, 200);
+        
+        //animate method. body's left offset will be set to equal to 0px, over 200ms.
+        //Not using a period before body. Why not?
+        $('body').animate({
+            left: '285px'
+        }, 200);
+    });
+    
+    $('.icon-close').click(function(){
+        $('.menu').animate({
+            left: '-285px'
+        }, 200);
+        
+        $('body').animate({
+            left: '0px'
+        }, 200);
+    });
+    
+    $('.icon-reveal').click(function(){
+        $('.menu').animate({
+            top: '100px'
+        }, 800);
+        $('.menu').animate({
+            top: '100px'
+        }, 800);
+        $('.menu').animate({
+            top: '0px'
+        }, 800);
+        
+        $('body').animate({
+            top: '100px'
+        }, 800);
+        $('body').animate({
+            top: '100px'
+        }, 800);
+        $('body').animate({
+            top: '0px'
+        }, 800);
+        
+        $('.top').animate({
+            top: '0px'
+        }, 800);
+        $('.top').animate({
+            top: '0px'
+        }, 800);
+        $('.top').animate({
+            top: '-100px'
+        }, 800);
+    });
+    
+    
+};
+
+// makes sure the web page has fully loaded before calling the main function.
+$(document).ready(main);
+
+
